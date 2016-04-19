91b3d5b4f51f5f5b9a0ed6fbe7f846a3a484e560
ycedenoo <ycedenoo@unal.edu.co>
2016-04-18 21:02:38 -0500

    En la Elaboracion de los Menus
    Menu Pausa y Menu Salir

---
+++ Assets/Scripts/BtnClic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnClic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+		//Application.LoadLevel("SomeLevel");
+
+
+	}
+
+	public void PressBtnMenu()
+	{
+		print ("Hola");
+		gameObject.SetActive (false);
+
+
+	}
+
+
+}
