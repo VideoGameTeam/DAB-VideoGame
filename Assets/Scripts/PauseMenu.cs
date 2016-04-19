da3a8b18ff2ac0153830c256e32ca74010f4fdfa
ycedenoo <ycedenoo@unal.edu.co>
2016-04-18 19:28:07 -0500

    Incluyendo el Menu Pausa
    Incluyendo el Menu Pausa, Agregada Escena Ingame

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	public GameObject objetpause;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) && Time.timeScale == 1) {
+			objetpause.SetActive (true);
+			Time.timeScale = 0;
+
+		} 
+		else if(Input.GetKeyDown (KeyCode.Escape) && Time.timeScale == 0) {
+			objetpause.SetActive (false);
+			Time.timeScale = 1;
+
+		}
+			
+	
+	}
+}
