bb2301b3b11335c3d9ee8707e5d2a74188538942
ycedenoo <ycedenoo@unal.edu.co>
2016-04-18 22:23:44 -0500

    Menus OK
    Esc Para Pausar

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PressBtnNewGame()
+	{
+		Time.timeScale = 1;
+		Application.LoadLevel ("InGame");
+
+	}
+
+
+
+}
