ffc963354eed35272258501b3ff6a7616aea422a
ycedenoo <ycedenoo@unal.edu.co>
2016-04-19 19:53:39 -0500

    Scripts de Menu Faltantes
    Pendientes Creditos y Continuar partida

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
+	public GameObject MenuPpal;
+	public GameObject MainOption;
+	public GameObject MainExit;
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +14,28 @@ public class MainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		/*
+		if (Input.GetKeyDown (KeyCode.Escape) && Time.timeScale == 1) {
+			MenuPpal.SetActive (false);
+			MainExit.SetActive (true);
+			//objectOptions.SetActive (false);
+			Time.timeScale = 0;
+
+		} 
+		else if(Input.GetKeyDown (KeyCode.Escape) && Time.timeScale == 0) {
+			if (objectExit.activeSelf||objectOptions.activeSelf) {
+				objectExit.SetActive (false);
+				objectOptions.SetActive (false);
+				objectpause.SetActive (true);
+
+			}
+			else {
+				objectpause.SetActive (false);
+				Time.timeScale = 1;
+			}
+
+		}
+	*/
 	}
 
 	public void PressBtnNewGame()
@@ -20,6 +45,50 @@ public class MainMenu : MonoBehaviour {
 
 	}
 
+
+	public void PressBtnMenuOpt()
+	{
+		MenuPpal.SetActive (false);
+		MainOption.SetActive (true);
+		//objectExit.SetActive (false);
+
+	}
+
+
+	public void PressBtnMenuExit()
+	{
+		MenuPpal.SetActive (false);
+		MainExit.SetActive (true);
+		//objectExit.SetActive (false);
+
+	}
+
+	public void PressBtnMenuCANCEL()
+	{
+		MenuPpal.SetActive (true);
+		MainOption.SetActive (false);
+		MainExit.SetActive (false);
+		//objectExit.SetActive (false);
+
+	}
+
+
+	public void PressBtnMenuExExit()
+	{
+		Time.timeScale = 0;
+
+	}
+
+
+	public void PressBtnMenuOptOK()
+	{
+
+		MenuPpal.SetActive (true);
+		MainOption.SetActive (false);
+		///GUARDAR CAMBIOS
+		
+
+	}
 
 
 }
