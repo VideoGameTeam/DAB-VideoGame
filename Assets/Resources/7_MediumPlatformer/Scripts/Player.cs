d5f2198e2d52a03dbc8722b18557a3c69d96f7ce
Alejandro Velasco <savelascod@unal.edu.co>
2016-05-01 11:31:13 -0500

    mejorada a animación de colgarse

--- Assets/Resources/7_MediumPlatformer/Scripts/Player.cs
+++ Assets/Resources/7_MediumPlatformer/Scripts/Player.cs
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour {
 	public Vector2 wallJumpOff;
 	public Vector2 wallLeap;
 
-	public float wallSlideSpeedMax = 3;
+	public float wallSlideSpeedMax = 2;
 	public float wallStickTime = .25f;
 	float timeToWallUnstick;
 
