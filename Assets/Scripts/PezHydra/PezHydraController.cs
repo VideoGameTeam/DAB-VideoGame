c5c16c9902bb53934ecc772e92c78522de411e66
mirag <mirag@DESKTOP-OP8TR6H>
2016-05-17 06:26:36 -0500

    Se corrige comportamiento de pez hydra, ya persigue, ataca, y regresa a su posición original apropiadamente.

--- Assets/Scripts/PezHydra/PezHydraController.cs
+++ Assets/Scripts/PezHydra/PezHydraController.cs
@@ -7,7 +7,7 @@ public class PezHydraController : MonoBehaviour
     public float Health;
 
     private Transform myTransform;
-	private Transform targetTransform;
+	public Transform targetTransform;
     private Vector3 originalPosition;
 	private LayerMask raycastLayer;
 	public float radius = 10;
@@ -27,7 +27,8 @@ public class PezHydraController : MonoBehaviour
     public float moveSpeed;
     public float maxFollowDistance;
     public float stoppingDistance;
-    private bool goingBack;
+    private bool goingBack = false;
+    private bool goingRight;
     float velocityXSmoothing;
     float jump;
     private float lastAttackTime;
@@ -55,47 +56,40 @@ public class PezHydraController : MonoBehaviour
     {
         if(targetTransform != null)
         {
-            MoveToTarget();
+            SetNavDestination(targetTransform.position);
+        }
+
+        if (goingBack)
+        {
+            print("Volviendo");
+            SetNavDestination(originalPosition);
         }
     }
 
-	void SearchForTarget()
-	{
-        if (targetTransform == null)
-		{
-			Collider2D hitCollider = Physics2D.OverlapCircle(myTransform.position, radius, raycastLayer);
 
-			if (hitCollider)
-			{
-				targetTransform = hitCollider.transform;
-				originTime = Time.time;
-			    lastAttackTime = 0;
-			}
-		}
-	}
-
-	void MoveToTarget()
-	{
-		if (targetTransform != null)
-		{
-			SetNavDestination(targetTransform.position);
-		}
-	}
+    void _SetOrientation(Vector2 input)
+    {
+        if(input.x >= 0){
+            modelGO.transform.rotation = Quaternion.Euler(0, 90, 0);
+            goingRight = true;
+        } else if (input.x < 0){
+            modelGO.transform.rotation = Quaternion.Euler(0, 270, 0);
+            goingRight = false;
+        }
+    }
 
     void SetNavDestination(Vector3 targetPosition)
     {
         Vector2 myPosition = myTransform.position;
 
-        if (Mathf.Abs(targetPosition.y - myPosition.y) > 2)
-        {
-            print("Distance to high");
+        if (Mathf.Abs(targetPosition.y - myPosition.y) > 5)
             return;
-        }
 
         if (targetTransform == null && Mathf.Abs(Vector3.Distance(myPosition, targetPosition)) <= stoppingDistance)
         {
+            velocity.x = 0;
+            velocity.y = 0;
             goingBack = false;
-            print("No need to move");
             return;
         }
 
@@ -108,32 +102,26 @@ public class PezHydraController : MonoBehaviour
         anim.wrapMode = WrapMode.Loop;
         anim.CrossFade("Walk");
 
-        // TODO Set rotation based on MonsterInput
-        print(myPosition.x);
-        print(targetPosition.x);
-        if (myPosition.x - targetPosition.x < 0)
+        if (myPosition.x < targetPosition.x)
         {
-            modelGO.transform.Rotate(0, 180, 0);
             MonsterInput.x = 0.5f;
             MonsterInput.y = 0;
         }
-        else
+
+        if (myPosition.x > targetPosition.x)
         {
-            modelGO.transform.Rotate(0, 180, 0);
             MonsterInput.x = -0.5f;
             MonsterInput.y = 0;
         }
 
+        _SetOrientation(MonsterInput);
+        
         int wallDirX = (controller.collisions.left) ? -1 : 1;
         float targetVelocityX = MonsterInput.x * moveSpeed;
         velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
 
-        bool wallSliding = false;
         if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
         {
-            print("wall sliding?");
-            wallSliding = true;
-
             if (velocity.y < -wallSlideSpeedMax)
             {
                 velocity.y = -wallSlideSpeedMax;
@@ -164,7 +152,6 @@ public class PezHydraController : MonoBehaviour
     {
         if (Time.time - lastAttackTime > attackTime)
         {
-            // TODO Fix error with animation iteration
             lastAttackTime = Time.time;
             anim.wrapMode = WrapMode.Once;
             anim.CrossFade("Attack");
@@ -185,11 +172,26 @@ public class PezHydraController : MonoBehaviour
         }
     }
 
-	IEnumerator DoCheck()
+    void _SearchForTarget()
+    {
+        if (targetTransform == null)
+        {
+            Collider2D hitCollider = Physics2D.OverlapCircle(myTransform.position, radius, raycastLayer);
+
+            if (hitCollider)
+            {
+                targetTransform = hitCollider.transform;
+                originTime = Time.time;
+                lastAttackTime = 0;
+            }
+        }
+    }
+
+    IEnumerator DoCheck()
 	{
         for (;;)
 		{
-			yield return new WaitForSeconds(1.0f);
+			yield return new WaitForSeconds(0.5f);
             bool checkIfTarget = false;
 
             if (Mathf.Abs(Vector3.Distance(originalPosition, myTransform.position)) >= maxFollowDistance)
@@ -204,12 +206,7 @@ public class PezHydraController : MonoBehaviour
 
             if (checkIfTarget && !goingBack)
             {
-                SearchForTarget();
-            }
-            else
-            {
-                print("Paso por aca");
-                SetNavDestination(originalPosition);
+                _SearchForTarget();
             }
 		}
 	}
