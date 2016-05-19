bbfe416b5fb2d24c223570d482e6031547446512
Alejandro Velasco <savelascod@unal.edu.co>
2016-05-19 13:02:21 -0500

    Nivel 1 carga nivel 2.

---
+++ Assets/Scripts/Levels/LoadLevel2.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel2 : MonoBehaviour {
+	void OnTriggerExit2D(Collider2D objeto){
+		SceneManager.LoadScene ("Level_2");
+	}
+}
