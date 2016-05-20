33874b269e64036ea85092713a7b6440ece781f0
Alejandro Velasco <savelascod@unal.edu.co>
2016-05-19 19:43:27 -0500

    Agregados textos

---
+++ Assets/Scripts/Elements/ShowText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ShowText : MonoBehaviour {
+
+	public string text;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Gamestate.EstadoJuego.ChangeMsg (text,5);
+		Object.Destroy (this.gameObject);
+	}
+}
