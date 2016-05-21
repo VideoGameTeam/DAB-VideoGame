0371114dd19248a3e6013abe2f7f2e462a763e99
Alejandro Velasco <savelascod@unal.edu.co>
2016-05-20 23:14:57 -0500

    Agregadas piedras de mana

---
+++ Assets/Scripts/Elements/ManaStone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManaStone : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			Gamestate.EstadoJuego.mana += 30;
+			if(Gamestate.EstadoJuego.mana>100){
+				Gamestate.EstadoJuego.mana = 100;
+			}
+			Object.Destroy (this.gameObject);
+
+		}
+	}
+}
