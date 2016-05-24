76e13eebd9aed6e65752d2a15e39a3bcd800d7e0
ycedenoo <ycedenoo@unal.edu.co>
2016-05-23 23:45:53 -0500

    Nuevo CheckPoint...  Animacion Btn Final

---
+++ Assets/Scripts/Elements/Puzzle.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle : MonoBehaviour {
+
+	public float PosYInit;
+	public GameObject Platform1;
+	public GameObject Platform2;
+	public GameObject Platform3;
+	public GameObject Platform4;
+	private bool btndown=false;
+
+	// Use this for initialization
+	void Start () {
+		//PosInit = new float[3];
+
+		PosYInit = gameObject.transform.position.y;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (btndown && gameObject.transform.position.y>(PosYInit-0.12f)) {
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x,( gameObject.transform.position.y-0.3f*Time.deltaTime), gameObject.transform.position.z);
+		
+		}
+
+		if (btndown==false && gameObject.transform.position.y<PosYInit) {
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x,( gameObject.transform.position.y+0.1f*Time.deltaTime), gameObject.transform.position.z);
+
+		}
+
+	
+	}
+
+
+
+	void OnTriggerEnter2D(Collider2D objeto)
+	{
+
+		if (objeto.tag == "Player") {
+
+			btndown = true;
+
+
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D objeto)
+	{
+
+		if (objeto.tag == "Player") {
+
+			btndown = false;
+
+
+		}
+	}
+
+
+
+
+
+}
---
+++ Assets/Scripts/Player/CheckPointOrbe.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointOrbe : MonoBehaviour {
+
+	private bool Checkp = false;
+
+	private float velrotate=700; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Checkp) {
+
+			gameObject.transform.Rotate(Vector3.down *velrotate* Time.deltaTime);
+			velrotate = velrotate + 20; 
+			if (velrotate > 2000) {
+				
+					Destroy (gameObject);
+
+			}
+		}
+			
+	
+	}
+
+
+
+
+	void OnTriggerEnter2D(Collider2D objeto)
+	{
+
+		if (objeto.tag == "Player") {
+
+			Gamestate.EstadoJuego.lastcheck [0]= objeto.gameObject.transform.position.x;
+			Gamestate.EstadoJuego.lastcheck [1]= objeto.gameObject.transform.position.y+2* objeto.gameObject.transform.localScale.y;
+			Gamestate.EstadoJuego.lastcheck [2]= objeto.gameObject.transform.position.z;
+
+			Checkp = true;
+
+
+
+
+		}
+
+	}
+
+}
